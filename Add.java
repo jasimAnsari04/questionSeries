4753bf42616312e1c4076249919cb68050815ad7
Md Jasim Ansari <jasimcec2025@gmail.com>
2025-07-03 11:00:18 +0530

    practice

---
+++ Add.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Add{
+    public static void main(String args[]){
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1+num2;
+        System.out.print(sum);
+    }
+}
