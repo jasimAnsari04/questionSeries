43ebc4a4d4c12f8b71d92dcfdb05fd419bb63b4d
Md Jasim Ansari <jasimcec2025@gmail.com>
2025-07-03 11:23:19 +0530

    java

---
+++ Swap.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Swap{
+    public static void main(String args[]){
+        // int a = 10;
+        // int b = 20;
+        // int temp;
+        // System.out.println("before Swapping: a = "+a +" b ="+b);
+        // temp = a;
+        // a = b;
+        // b = temp;
+        // System.out.println("a = "+a+"\nb = "+b);
+
+
+
+
+        // int a = 10;
+        // int b = 20;
+        // System.out.println("before Swapping: a = "+a +" b ="+b);
+        // a = a+b;
+        // b = a-b;
+        // a = a-b;
+        // System.out.println("after swapping: a = "+a+" b = "+b);
+
+    }
+}
